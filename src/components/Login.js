d1a782c2898e6b5e6af479dcccb1900d7adec37b
Devin Sherman <fidalgo15@Devins-MacBook-Pro.local>
2019-06-20 19:43:28 -0600

    Login form added

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,7 +3,14 @@ import React from 'react';
 export default class extends React.Component {
     render() {
         return(
-            
+            <>
+                <h1>Login Page</h1>
+                <form>
+                    <p>User Name: </p>
+                    <input type="text" placeholder="Name Please..." Required/>
+                </form>
+
+            </>
         )
     }
 }
