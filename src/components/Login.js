50ff62b2ef0a02935ec636911f48968b126cd350
Devin Sherman <fidalgo15@Devins-MacBook-Pro.local>
2019-06-25 18:10:41 -0600

    onSubmit change

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,17 +2,35 @@ import React from 'react';
 
 export default class extends React.Component {
     state = {
-        name: 'Devin'
+        name: 'Devin',
+        pass: '*****',
     }
     render() {
         console.log(this.state.name)
-        return(
+
+        return (
             <>
                 <h1>Login Page</h1>
-                <form>
+                <form onSubmit={(event) => {
+                    event.preventDefault()
+                    alert(this.state.name)
+                }}>
                     <p>User Name: </p>
-                    <input type="text" placeholder="Name Please..." onChange={(event) => this.setState({name: event.target.value})} required/>
+                    <input name="username" type="text" placeholder="Name Please..." onChange={(event) => this.setState({ name: event.target.value })} required 
+                    value={this.state.name}/>
+                    
+                <br />
+
+                    <p>Password: </p>
+                    <input name="password" type="password" placeholder="Password" onChange={(event) => this.setState({ pass: event.target.value })} required />
+                    <br />
+                    <input type="submit" value="Submit" />
                 </form>
+
+                
+
+                {/* <p>{this.state.name}</p>
+                <p>{this.state.pass}</p> */}
 
             </>
         )
